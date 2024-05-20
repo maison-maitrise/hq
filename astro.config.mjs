98197b87c57f2c8aa6c38b848329d123b551fc0c
messiahsmiley <messiah.smiley@sologic.com>
2024-05-20 06:08:20 -0400

    Added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/static";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: "static",
+  adapter: vercel(),
 });
