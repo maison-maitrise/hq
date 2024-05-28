71a440f25843ab04817cb502267a8324f29c9d8d
messiahsmiley <messiah.smiley@sologic.com>
2024-05-28 01:22:21 -0400

    Added compressor and shared asset lib

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/static";
-
 import react from "@astrojs/react";
+
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), playformCompress()],
   output: "static",
-  adapter: vercel(),
+  adapter: vercel()
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,39 +1,4 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    colors: {
-      ice: "#96daff",
-      snow: "#dffcff",
-      mars: "#e6bd9e",
-      lipstick: "#cd0000",
-      "red-revolt": "#f00000",
-      "tender-pink": "#E2A5A5",
-      noir: "#0F0F0F"
-    },
-    fontFamily: {
-      bauer: "'Bauer Bodni', serif",
-      maison: "'Maison Neue', sans serif",
-      nympha: "Nympha, serif",
-    },
-    fontWeight: {
-      bold: 700,
-      demi: 600,
-      medium: 500,
-      normal: 400,
-      light: 300,
-      xlight: 200,
-      thin: 100,
-      black: 900,
-    },
-    extend: {
-      backgroundImage: {
-        "pattern-yellow": "url('/assets/bg.jpg')",
-        "pattern-black": "url('/assets/bg-black.jpg')",
-        "pattern-red": "url('/assets/bg-red.jpg')",
-        "pattern-blue": "url('/assets/bg-blue.jpg')",
-      },
-    },
-  },
-  plugins: [],
-};
+// tailwind.config.js in your project
+import defaultConfig from "valentinesharedassets/tailwind.config.js";
+
+export default defaultConfig;
