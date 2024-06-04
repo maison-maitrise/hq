4e0991481d0298a03b646a35894a18099b060db5
messiahsmiley <messiah.smiley@sologic.com>
2024-06-04 19:12:59 -0400

    Added autoprefixer

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,19 @@ import vercel from "@astrojs/vercel/static";
 import react from "@astrojs/react";
 
 import playformCompress from "@playform/compress";
+import postcss from "@astrojs/postcss";
+import autoprefixer from "autoprefixer";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), playformCompress()],
+  integrations: [tailwind(), react(), playformCompress(), postcss()],
   output: "static",
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
+  vite: {
+    postcss: {
+      plugins: [autoprefixer({})],
+    },
+  },
 });
